733882f0b9f2b08f8968d6d690ccd082ce233a6b
Piotr Pirański <piotr.piranski@gmail.com>
2019-03-27 23:22:30 +0100

    repair about section

--- src/js/o_nas.js
+++ src/js/o_nas.js
@@ -4,37 +4,16 @@ console.log("about");
     aboutText();
 
     function aboutText(){
-        const aboutTextHiddenSimon = document.querySelector(".aboutTextSimon-hidden");
-        const aboutTextHiddenPeter = document.querySelector(".aboutTextPeter-hidden");
-        const aboutPhotoSimon = document.querySelector(".aboutPhotoSimon-show")
-        const aboutPhotoPeter = document.querySelector(".aboutPhotoPeter-show")
-        const simonIMG = document.querySelector(".simonImg");
-        const peterIMG = document.querySelector(".peterImg");
-        const simonP = document.querySelector(".simonP");
-        const peterP = document.querySelector(".peterP");
+        $(".tapDivSimon").click(function(){
+            $(".simonP").slideToggle("slow");
+            $(".tapDivSimon").toggleClass("after")
+        }); 
 
-
-        simonIMG.addEventListener("click", function(){
-            aboutTextHiddenSimon.classList.toggle("show")
-            aboutPhotoSimon.classList.toggle("hidden")
-        });
-
-        simonP.addEventListener("click", function(){
-
-            aboutTextHiddenSimon.classList.remove("show");
-            aboutPhotoSimon.classList.remove("hidden");
-        });
-
-        peterIMG.addEventListener("click", function(){
-            aboutTextHiddenPeter.classList.toggle("show")
-            aboutPhotoPeter.classList.toggle("hidden")
-        });
-
-        peterP.addEventListener("click", function(){
-
-            aboutTextHiddenPeter.classList.remove("show");
-            aboutPhotoPeter.classList.remove("hidden");
+        $(".tapDivPeter").click(function(){
+            $(".peterP").slideToggle("slow");
+            $(".tapDivPeter").toggleClass("after")
         });
     }
+
 })
 
